999bf7efc120c6d3c8fa7ec14b4658531f450768
Jihunkim95 <darak551@naver.com>
2024-05-29 16:34:45 +0900

    fix: url경로 오류 수정

--- src/examples/footers/FooterDefault.vue
+++ src/examples/footers/FooterDefault.vue
@@ -53,7 +53,7 @@ defineProps({
         items: [
           {
             name: "about us",
-            href: "https://www.platformlogics.com/pages/landing-pages/about-us"
+            href: "https://platformlogics.com/pages/landing-pages/about-us"
           },
           // {
           //   name: "freebies",
